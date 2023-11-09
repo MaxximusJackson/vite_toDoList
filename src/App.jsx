16764c7dcae9021fbafe139baba3c57911ab0cd2
MaxximusJackson <111911331+MaxximusJackson@users.noreply.github.com>
2023-11-09 11:49:29 -0600

    Save

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>To Do List App</h1>
+      
+    </>
+  )
+}
+
+export default App
